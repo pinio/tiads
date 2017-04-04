59f536653e41f467411f87ed00066c3bc7e338ac
Ygor Lemos <ygbr@mac.com>
2017-04-04 18:22:57 -0300

    Updated sample app for Android

--- example/app.js
+++ example/app.js
@@ -1,15 +1,16 @@
 Ti.UI.setBackgroundColor('#fff');
 
 var w = Ti.UI.createWindow({
-    title: "Identifiers",
+    title: "Ads Identifiers",
     layout: "vertical"
     
 });
 
 var tiads = require("ninja.ygor.tiads");
 
+
 var lbl = Ti.UI.createLabel({
-    text: "IDFA",
+    text: "advertisingIdentifier",
     top: 40
 });
 
@@ -17,7 +18,7 @@ w.add(lbl);
 
 lbl.addEventListener("click", function() {
         var idfa = tiads.advertisingIdentifier;
-        lbl.setText("IDFA:" + idfa);
+        lbl.setText("advertisingIdentifier: " + idfa);
 });
 
 var lbl2 = Ti.UI.createLabel({
@@ -28,7 +29,7 @@ w.add(lbl2);
 
 lbl2.addEventListener("click", function() {
         var iidfa = tiads.isAdvertisingTrackingEnabled;
-        lbl2.setText("isAdvertisingTrackingEnabled:" + iidfa);
+        lbl2.setText("isAdvertisingTrackingEnabled: " + iidfa);
 })
 
 var lbl3 = Ti.UI.createLabel({
@@ -39,7 +40,18 @@ w.add(lbl3);
 
 lbl3.addEventListener("click", function() {
         var idfv = tiads.identifierForVendor;
-        lbl3.setText("identifierForVendor:" + idfv);
+        lbl3.setText("identifierForVendor: " + idfv);
+})
+
+var lbl4 = Ti.UI.createLabel({
+    text: "getAdvertisingIdInfo"
+});
+
+w.add(lbl4);
+
+lbl4.addEventListener("click", function() {
+        var idfv = tiads.getAdvertisingIdInfo();
+        lbl4.setText("getAdvertisingIdInfo: " + idfv);
 })
 
 w.open();
