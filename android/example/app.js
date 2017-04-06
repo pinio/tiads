d22a26f9ece2b16f24173ba004f07e812b97aa18
Ygor Lemos <ygbr@mac.com>
2017-04-05 21:28:17 -0300

    Bump to 1.1.0 and updated the docs for new Android methods

--- android/example/app.js
+++ android/example/app.js
@@ -1,57 +1,85 @@
 Ti.UI.setBackgroundColor('#fff');
 
+var tiads = require("ninja.ygor.tiads");
+
 var w = Ti.UI.createWindow({
-    title: "Ads Identifiers",
+    title: "Identifiers",
     layout: "vertical"
     
 });
 
-var tiads = require("ninja.ygor.tiads");
+if (Ti.Platform.osname == "android") {
+    var lblGAI = Ti.UI.createLabel({
+        text: "getAdvertisingIdentifier"
+    });
 
+    w.add(lblGAI);
 
-var lbl = Ti.UI.createLabel({
-    text: "advertisingIdentifier",
-    top: 40
-});
-
-w.add(lbl);
-
-lbl.addEventListener("click", function() {
-        var idfa = tiads.advertisingIdentifier;
-        lbl.setText("advertisingIdentifier: " + idfa);
-});
-
-var lbl2 = Ti.UI.createLabel({
-    text: "isAdvertisingTrackingEnabled"
-});
-
-w.add(lbl2);
-
-lbl2.addEventListener("click", function() {
-        var iidfa = tiads.isAdvertisingTrackingEnabled;
-        lbl2.setText("isAdvertisingTrackingEnabled: " + iidfa);
-})
-
-var lbl3 = Ti.UI.createLabel({
-    text: "identifierForVendor"
-});
-
-w.add(lbl3);
-
-lbl3.addEventListener("click", function() {
-        var idfv = tiads.identifierForVendor;
-        lbl3.setText("identifierForVendor: " + idfv);
-})
-
-var lbl4 = Ti.UI.createLabel({
-    text: "getAdvertisingIdInfo"
-});
-
-w.add(lbl4);
-
-lbl4.addEventListener("click", function() {
+    lblGAI.addEventListener("click", function() {
         var idfv = tiads.getAdvertisingIdInfo();
-        lbl4.setText("getAdvertisingIdInfo: " + idfv);
-})
+        lblGAI.setText("getAdvertisingIdentifier: " + idfv);
+    })
+    
+    var lblLAE = Ti.UI.createLabel({
+        text: "isLimitAdTrackingEnabled"
+    });
+
+    w.add(lblLAE);
+
+    lblLAE.addEventListener("click", function() {
+        var isLimitAdTrackingEnabled = tiads.isLimitAdTrackingEnabled();
+        lblLAE.setText("isLimitAdTrackingEnabled: " + isLimitAdTrackingEnabled);
+    })
+
+    var lblAID = Ti.UI.createLabel({
+        text: "getAndroidID"
+    });
+
+    w.add(lblAID);
+
+    lblAID.addEventListener("click", function() {
+        var androidid = tiads.getAndroidID();
+        lblAID.setText("getAndroidID: " + androidid);
+    })
+
+    
+    
+} else {
+    var lblADID = Ti.UI.createLabel({
+        text: "advertisingIdentifier",
+        top: 40
+    });
+
+    w.add(lblADID);
+
+    lblADID.addEventListener("click", function() {
+        var idfa = tiads.advertisingIdentifier;
+        lblADID.setText("advertisingIdentifier: " + idfa);
+    });
+
+    var lblATE = Ti.UI.createLabel({
+        text: "isAdvertisingTrackingEnabled"
+    });
+
+    w.add(lblATE);
+
+    lblATE.addEventListener("click", function() {
+        var iidfa = tiads.isAdvertisingTrackingEnabled;
+        lblATE.setText("isAdvertisingTrackingEnabled: " + iidfa);
+    })
+
+    var lblIFV = Ti.UI.createLabel({
+        text: "identifierForVendor"
+    });
+
+    w.add(lblIFV);
+
+    lblIFV.addEventListener("click", function() {
+        var idfv = tiads.identifierForVendor;
+        lblIFV.setText("identifierForVendor: " + idfv);
+    })
+    
+}
+
 
 w.open();
