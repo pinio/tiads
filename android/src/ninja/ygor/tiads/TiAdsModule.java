e5b9b492af115194406d48e2573c792837c45a60
Ygor Lemos <ygbr@mac.com>
2020-06-11 00:02:31 -0300

    Add some linting to TiAdsModule.java

--- android/src/ninja/ygor/tiads/TiAdsModule.java
+++ android/src/ninja/ygor/tiads/TiAdsModule.java
@@ -10,35 +10,32 @@ import org.appcelerator.kroll.common.TiConfig;
 
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient.Info;
-//import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
-//import com.google.android.gms.common.GooglePlayServicesUtil;
-//import java.io.IOException;
 
 import android.os.AsyncTask;
 
-
-@Kroll.module(name="TiAds", id="ninja.ygor.tiads")
+@Kroll.module(name = "TiAds", id = "ninja.ygor.tiads")
 public class TiAdsModule extends KrollModule {
 
     // Standard Debugging variables
     private static final String LCAT = "TiAdsModule";
     private static final boolean DBG = TiConfig.LOGD;
-    
+
     // ADID State Variables
     public static String adidResult = "";
     public static boolean adidLimit = false;
-    
+
     public TiAdsModule() {
         super();
     }
-    
+
     private static class AdvertisingIdTask extends AsyncTask<Void, Void, AdvertisingIdClient.Info> {
         @Override
         protected AdvertisingIdClient.Info doInBackground(Void... params) {
             try {
-                AdvertisingIdClient.Info idInfo = AdvertisingIdClient.getAdvertisingIdInfo(TiApplication.getInstance().getApplicationContext());
+                AdvertisingIdClient.Info idInfo = AdvertisingIdClient
+                        .getAdvertisingIdInfo(TiApplication.getInstance().getApplicationContext());
                 return idInfo;
             } catch (GooglePlayServicesNotAvailableException e) {
                 e.printStackTrace();
@@ -47,18 +44,18 @@ public class TiAdsModule extends KrollModule {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            
+
             return null;
         }
+
         @Override
         protected void onPostExecute(AdvertisingIdClient.Info advertId) {
             adidResult = advertId.getId();
             adidLimit = advertId.isLimitAdTrackingEnabled();
         }
-       
+
     };
-    
-    
+
     @Kroll.onAppCreate
     public static void onAppCreate(TiApplication app) {
         Log.d(LCAT, "Initializing tiads module.");
@@ -66,19 +63,20 @@ public class TiAdsModule extends KrollModule {
     }
 
     // Methods
-    
+
     // Get ADID - Android Advertising ID
-    @Kroll.getProperty @Kroll.method
+    @Kroll.getProperty
+    @Kroll.method
     public String getAdvertisingIdInfo() {
         new AdvertisingIdTask().execute();
         return adidResult;
     }
-    
-    @Kroll.getProperty @Kroll.method
+
+    @Kroll.getProperty
+    @Kroll.method
     public boolean isLimitAdTrackingEnabled() {
         new AdvertisingIdTask().execute();
         return adidLimit;
     }
 
 }
-
